73f955c513993b82ba54d246f8381ecf49c68876
TracyWafula <tracywafula@gmail.com>
2026-04-12 20:12:01 +0200

    Made changes to the lab

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Write your solution in this file!
+const burgers = ['Hamburger', 'Cheeseburger'];
+let featuredDrink = 'Strawberry Milkshake';
+
+function addBurger(burgers){
+    const newBurger = 'Flatburger';
+    burgers.push(newBurger);
+    return burgers;
+
+}
+if(true){
+        const anotherNewBurger='Maple Bacon Burger';
+        burgers.push(anotherNewBurger);
+}
+console.log(addBurger(burgers));
+
+function changeFeaturedDrink(featuredDrink){
+    featuredDrink = 'The JavaShake' ;
+    console.log(featuredDrink)
+}
+changeFeaturedDrink();
